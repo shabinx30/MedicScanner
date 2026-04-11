9652d320287898218e57c5b97af0a50f0c24d781
shabinx30 <shabeensharih@gmail.com>
2026-04-11 21:04:55 +0530

    refine: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,22 +2,32 @@ import Express, { type Application } from "express";
 import searchRouter from "./modules/search/search.route.js";
 import { connectDB } from "./config/db.js";
 import ocrRouter from "./modules/ocr/ocr.route.js";
-import cors from "cors"
+import cors from "cors";
 import cronRouter from "./modules/cron/cron.route.js";
+import { allowedOrigins } from "./config/allowedOrigins.js";
 
 const app: Application = Express();
 
 const corsOptions = {
-    origin: "http://localhost:3000",
+    origin: (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     methods: ["POST"],
     allowedHeaders: ["Content-Type"],
     credentials: false,
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(Express.json({limit: "50mb"}));
-app.use(Express.urlencoded({ limit: '50mb', extended: true }));
+app.use(Express.json({ limit: "50mb" }));
+app.use(Express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use("/", searchRouter);
 app.use("/ocr", ocrRouter);
---
+++ backend/src/config/allowedOrigins.ts
@@ -0,0 +1,4 @@
+export const allowedOrigins = [
+    process.env.FRONTEND_URL_1,
+    process.env.FRONTEND_URL_2,
+];
