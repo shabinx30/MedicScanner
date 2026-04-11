06bf1b386a3b8b3e13e54f08aa7c8c96d30e25cb
shabinx30 <shabeensharih@gmail.com>
2026-04-11 15:00:00 +0530

    feat: vercel cron job

---
+++ frontend/app/api/cron/add-new-list/route.ts
@@ -0,0 +1,15 @@
+import { after } from "next/server";
+
+export async function Get() {
+    if (!process.env.API_URL) {
+        throw new Error("could not find api url");
+    }
+
+    after(() => {
+        fetch(process.env.API_URL + "/cron/add-new-list", {
+            method: "GET",
+        }).catch((error) => console.log(error));
+    });
+
+    return new Response("Triggered", { status: 200 });
+}
